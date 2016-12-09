cf2524358a3a5d2c70dfad6a72c6f1e98e7d31a0
Thum Wei Wah <thumweiwah@TFS-MACM-007.local>
2016-12-09 11:48:48 +0800

    added a test class

---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Thum Wei Wah on 09/12/2016.
+//  Copyright © 2016 Thum Wei Wah. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
